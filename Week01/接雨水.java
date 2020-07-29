f5fad48338ce848f739a778a0fa80157593a2cf5
SIE-ZHANGJUN <rockyseven@163.com>
2020-07-29 17:16:20 +0800

    接雨水

--- Week01/接雨水.java
+++ Week01/接雨水.java
@@ -19,17 +19,16 @@ import com.sun.source.tree.AssignmentTree;
 class Solution {
 	public int trap(int[] height) {
 		//双指针解法
-		int max_left = 0;
 		int sum = 0;
-		int[] max_right = new int[height.lengh];
-		for(int i = height.length - 2;i >= 0;i--){
-			max_right[i] = Math.max(max_right[i+1],height[i+1]);
+		int max_left = 0;
+		int[] max_right = new int[height.length];
+		for (int i = height.length - 2; i >= 0; i--) {
+			max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
 		}
-
-		for(int i = 0; i < height.length-1; i++){
-			max_left = Math.max(max_left,height[i-1]);
-			int min = Math.max(max_left, max_right[i]);
-			if(min > height[i]){
+		for (int i = 1; i < height.length - 1; i++) {
+			max_left = Math.max(max_left, height[i - 1]);
+			int min = Math.min(max_left, max_right[i]);
+			if (min > height[i]) {
 				sum = sum + (min - height[i]);
 			}
 		}
